e26a5821bc41de30f4ea817e71e1f7f8c60ea940
Thiago Naves <naves.thiago@gmail.com>
2018-10-10 00:37:04 -0300

    Added option to select different test functions

--- t2/equal.c
+++ t2/equal.c
@@ -1,25 +1,45 @@
 #include "quadrature.h"
 #include <stdio.h>
+#include <stdlib.h>
 #include <mpi.h>
 #include <math.h>
 #ifndef M_PI
 #define M_PI 3.14159265358979323846
 #endif
 
-static const double A = -5;
-static const double B = 5;
+/**
+ * @brief This struct stores a test function pointer
+ * and the integration interval
+ */
+typedef struct {
+	double start;
+	double end;
+	func_t f;
+} test_function_t;
 
 static double f_test4(double x) {
 	return x*sin(x)+x*x*sin(10*x+M_PI/8.0)+2*sin(13*x+M_PI/3.0)+(x+3)*(x+3)/4.0;
 }
+
+/**
+ * @brief List of available tests (selected via the first command line argument
+ */
+static const test_function_t tests[] = {
+	{.start = -5, .end = 5, .f = f_test4}
+};
+
+/**
+ * @brief User selected test
+ */
+static const test_function_t * test;
 
 void main_master(void) {
 	int numprocs;
 	MPI_Comm_size(MPI_COMM_WORLD, &numprocs);
 	int intervals = numprocs - 1;
 	printf("Main: %d intervals\n", intervals);
-	double step = fabs(B - A) / intervals;
-	double a = A;
+	double step = fabs(test->end - test->start) / intervals;
+	double a = test->start;
 	double area = 0;
 	for (int i=1; i<=intervals; i++) {
 		double interval[2];
@@ -27,7 +47,7 @@ void main_master(void) {
 		if (i != intervals)
 			interval[1] = a + step;
 		else
-			interval[1] = B; // Avoid rounding errors
+			interval[1] = test->end; // Avoid rounding errors
 
 		if (MPI_Send(&interval, 2, MPI_DOUBLE, i, 0, MPI_COMM_WORLD) != MPI_SUCCESS)
 			MPI_Abort(MPI_COMM_WORLD, 1);
@@ -52,13 +72,23 @@ void main_worker(void) {
 		MPI_Abort(MPI_COMM_WORLD, 2);
 
 	printf("Worker: [%f, %f]\n", interval[0], interval[1]);
-	double area = integrate(f_test4, interval[0], interval[1]);
+	double area = integrate(test->f, interval[0], interval[1]);
 	if (MPI_Send(&area, 1, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD) != MPI_SUCCESS)
 		MPI_Abort(MPI_COMM_WORLD, 3);
 	printf("Worker: area = %f\n", area);
 }
 
 int main(int argc, char ** argv) {
+	unsigned test_id = 0;
+	if (argc > 1)
+		test_id = atoi(argv[1]);
+
+	if (test_id > sizeof(tests)/sizeof(tests[0])) {
+		printf("Invalid test number\n");
+		return 4;
+	}
+
+	test = &tests[test_id];
 	int procid;
 	MPI_Init(&argc, &argv);
 	MPI_Comm_rank(MPI_COMM_WORLD, &procid);
