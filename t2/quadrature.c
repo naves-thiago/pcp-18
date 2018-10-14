5aa9171ad6cad6f60c9a20f69fe3d574d448f4bf
Thiago Naves <naves.thiago@gmail.com>
2018-10-14 20:01:33 -0300

    Move quadraure precision and calc_area into .h

--- t2/quadrature.c
+++ t2/quadrature.c
@@ -1,12 +1,6 @@
 #include "quadrature.h"
 #include <math.h>
 
-static const double precision = 0.0000000000000001; // 10^-16
-
-static inline double calc_area(func_t f, double a, double b) {
-	return (f(a) + f(b)) * fabs(b-a) * 0.5;
-}
-
 static double integrate_recursive(func_t f, double a, double b, double area) {
 	double mid = (a + b) / 2.0;
 	double area_left = calc_area(f, a, mid);
--- t2/quadrature.h
+++ t2/quadrature.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <math.h>
 
 /**
  * @brief Function pointer representing the function to be integrated
@@ -9,3 +10,15 @@ typedef double (*func_t)(double);
  * @brief Calculate the integral of f in the [a,b] interval
  */
 double integrate(func_t f, double a, double b);
+
+/**
+ * @brief Maximum allowed difference in area
+ */
+static const double precision = 0.0000000000000001; // 10^-16
+
+/**
+ * @brief Calculate the area of the trapezoid
+ */
+static inline double calc_area(func_t f, double a, double b) {
+	return (f(a) + f(b)) * fabs(b-a) * 0.5;
+}
