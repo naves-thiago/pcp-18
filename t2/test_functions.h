7a230dd13f015ee4a037c57ee57c99e697d21e80
Thiago Naves <naves.thiago@gmail.com>
2018-10-19 01:25:57 -0300

    New test functions

--- t2/test_functions.h
+++ t2/test_functions.h
@@ -16,15 +16,33 @@ typedef struct {
 	func_t f;
 } test_function_t;
 
-static double f_test4(double x) {
+static double f0(double x) {
 	return x*sin(x)+x*x*sin(10*x+M_PI/8.0)+2*sin(13*x+M_PI/3.0)+(x+3)*(x+3)/4.0;
+}
+
+static double f1(double x) {
+// sum[0.1*cos(x*y^2/40), {y,0,10}] ; x=-30..30
+	double tmp =  0;
+	for (int y=0; y<=10; y++)
+		tmp += 0.1*cos(x*y*y/40.0);
+	return tmp;
+}
+
+static double f2(double x) {
+// sum[0.1*cos(x*y^2/40), {y,0,10}] ; x=-30..30
+	double tmp =  0;
+	for (int y=0; y<=50; y++)
+		tmp += 0.1*cos(x*y*y/40.0);
+	return tmp;
 }
 
 /**
  * @brief List of available tests
  */
 static const test_function_t tests[] = {
-	{.start = -5, .end = 5, .f = f_test4}
+	{.start = -5, .end = 5, .f = f0},
+	{.start = -30, .end = 30, .f = f1},
+	{.start = -10, .end = 10, .f = f2},
 };
 
 #define NTESTS (sizeof(tests) / sizeof(tests[0]))
